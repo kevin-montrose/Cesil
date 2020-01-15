4c2ea55d2cff8de2f3dd118a4eb32db1da78f437
kevin-montrose <kevin.montrose@gmail.com>
2020-01-14 21:03:16 -0500

    regenerate docs, add a root redirect

--- GenerateDocs.bat
+++ GenerateDocs.bat
@@ -1,3 +1,11 @@
 @ECHO OFF
 
 .\docfx\docfx.exe .\docfx.json
+
+(
+	echo ^<html^>
+	echo ^<head^>
+	echo ^<meta http-equiv="refresh" content="0;https://kevin-montrose.github.io/Cesil/api/Cesil.html" /^>
+	echo ^</head^>
+	echo ^</html^>
+) > .\docs\index.html
