2c2a7d8010943bb7438671146faf366da3e76272
¯\_(ツ)_/¯ <Siliconrob@siliconheaven.net>
2021-02-21 15:28:28 -0600

    Convert batch files to powershell (#46)

---
+++ RunCodeCoverage.ps1
@@ -0,0 +1,21 @@
+Write-Output 'Cleaning up old results'
+
+Remove-Item -Path '.\TestCoverageResults\*' -Recurse -Force
+
+Write-Output 'This folder is here to receive code coverage results.  Ignore this file.>.\TestCoverageResults\README.txt'
+
+Start-Process -NoNewWindow -FilePath 'dotnet' -ArgumentList 'test','/p:CollectCoverage=true','/p:CoverletOutputFormat=opencover','/p:CoverletOutput=.\..\TestCoverageResults\Coverage.xml','/p:SingleHit=true'
+
+Write-Output 'Coverage complete'
+
+Start-Process -NoNewWindow -FilePath 'dotnet' -ArgumentList '.\ReportGenerator\ReportGenerator.dll','-reports:.\TestCoverageResults\Coverage.xml','-targetdir:.\TestCoverageResults\'
+
+if ($process.ExitCode -gt 0) {
+	return
+}
+
+Write-Output 'Report generated'
+
+if ($param1=$args[0] -ne 'silent') {
+	Start-Process -FilePath '.\TestCoverageResults\index.htm' 
+}
